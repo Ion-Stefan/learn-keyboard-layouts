63ca9b626507b633e18c2d925bd87fdf68064dd1
Ion Stefan <ion.adrian.stefann@gmail.com>
2024-03-28 13:31:55 +0100

    initialized project

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <div className='flex justify-between py-8 px-16'>
+      <p>logo</p>
+      <div className='flex gap-8'>
+        <p>about</p>
+        <p>ceva</p>
+        <p>iarceva</p>
+      </div>
+    </div>
+  )
+}
